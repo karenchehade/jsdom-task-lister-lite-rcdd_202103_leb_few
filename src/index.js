6b4be68af6498df738d3d8a2d7544b900042729f
karen chehade <karen_chehade@hotmail.com>
2021-04-15 08:21:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const task = [];
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e)=>{
+    const newTask = document.getElementById("new-task-description").value;
+    task.push(newTask);
+    const addTask = document.getElementById("tasks");
+    addTask.innterHTML += "<br>";
+    addTask.innerText += newTask + " ";
+    event.preventDefault();
+  })
 });
